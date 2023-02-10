c9b074a98ff2cc2c7d1e46c367755aa270cbbf47
Asova417 <asova417@gmail.com>
2023-02-10 23:40:09 +0500

    Выдаёт дни недели по заданному номеру

---
+++ c#/seminar/Urok_01/Example_03/Program.cs
@@ -0,0 +1,47 @@
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number == 1)
+
+{
+    Console.WriteLine($"{number} = понедельник");
+}
+else if (number == 2)
+
+{
+    Console.WriteLine($"{number} = вторник");
+}
+else if (number == 3)
+
+{
+    Console.WriteLine($"{number} = среда");
+}
+else if (number == 4)
+
+{
+    Console.WriteLine($"{number} = четверг");
+}
+else if (number == 5)
+
+{
+    Console.WriteLine($"{number} = пятница");
+}
+else if (number == 6)
+
+{
+    Console.WriteLine($"{number} = суббота");
+}
+else if (number == 7)
+
+{
+    Console.WriteLine($"{number} = воскресенье");
+}
+else
+{
+    Console.WriteLine($"{number} = такого дня недели нет");
+}
