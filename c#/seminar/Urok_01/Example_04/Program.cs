1535c5d3f7ae6c0c83c6dcd93efe46206bc35a81
Asova417 <asova417@gmail.com>
2023-02-11 00:26:33 +0500

    от -N до N. С решением от преподавателя.

--- c#/seminar/Urok_01/Example_04/Program.cs
+++ c#/seminar/Urok_01/Example_04/Program.cs
@@ -5,12 +5,21 @@
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 //2 -> " -2, -1, 0, 1, 2"
 
-Console.Write("Введите число: ");
-int numberA = Convert.ToInt32 (Console.ReadLine());
-int numberB = numberA - (numberA*2);
+//Console.Write("Введите число: ");
+//int numberA = Convert.ToInt32 (Console.ReadLine());
+//int numberB = numberA - (numberA*2);
 
-while (numberB <= numberA)
+//while (numberB <= numberA)
+//{
+//    Console.Write(numberB+ "  ");
+//    numberB++;
+//}
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32 (Console.ReadLine());
+int temp = -N; //*(-1) сокращение
+while(temp <= N)
 {
-    Console.Write(numberB+ "  ");
-    numberB++;
+    Console.Write($"{temp} ");
+    temp++; //temp = temp +1
 }
