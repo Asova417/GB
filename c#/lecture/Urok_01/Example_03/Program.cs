233534128618e22bc120dd0f138414ff574c32da
Asova42 <asova417@gmail.com>
2023-02-10 18:43:58 +0500

    RandomSum

--- c#/lecture/Urok_01/Example_03/Program.cs
+++ c#/lecture/Urok_01/Example_03/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
 Console.WriteLine(numberA+numberB);
 
 // Второй вариант
