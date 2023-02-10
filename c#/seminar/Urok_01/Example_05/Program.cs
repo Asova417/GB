5de84a07dc48ec7776e9300af3f970f5ad60b9e7
Asova417 <asova417@gmail.com>
2023-02-11 00:14:27 +0500

    принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.

---
+++ c#/seminar/Urok_01/Example_05/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//	456 -> 6
+//	782 -> 2
+//	918 -> 8
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32 (Console.ReadLine());
+int numberB = numberA%10;
+Console.Write(numberB);
