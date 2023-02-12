f22bde2aee8d0eba3e2c3b383efa5c75576b6474
Asova417 <asova417@gmail.com>
2023-02-12 14:55:07 +0500

    Поиск максимального с приминением метода/функции

--- c#/lecture/Urok_02/Example_03/Program.cs
+++ c#/lecture/Urok_02/Example_03/Program.cs
@@ -2,7 +2,7 @@
 int Max (int arg1, int arg2, int arg3)
 {
     int result = arg1;
-    if (arg2 > result) result = arg2;
+    if (arg2 > result) result = arg2;      
     if (arg3 > result) result = arg3;
     return result;
 }
@@ -19,13 +19,11 @@ int a3 = 133;
 int b3 = 23;
 int c3 = 33;
 
-//int max1 = Max(a1, b1, c1);
-//int max2 = Max(a2, b2, c2);
-//int max3 = Max(a3, b3, c3);
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
 
-//int max = Max(max1, max2, max3);
-
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+int max = Max(max1, max2, max3);
 
 
 Console.WriteLine(max);
