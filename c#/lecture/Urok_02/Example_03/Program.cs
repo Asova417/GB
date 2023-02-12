d631cfd19d8bb0204c9ecee5f70297c402b5c50c
Asova417 <asova417@gmail.com>
2023-02-12 15:22:53 +0500

    Поиск максимального с использованием метода + массив

--- c#/lecture/Urok_02/Example_03/Program.cs
+++ c#/lecture/Urok_02/Example_03/Program.cs
@@ -1,4 +1,4 @@
-﻿//Поиск максимального значения с использованием метода/функции.
+﻿//Поиск максимального значения с использованием метода/функции + массив.
 int Max (int arg1, int arg2, int arg3)
 {
     int result = arg1;
@@ -6,24 +6,14 @@ int Max (int arg1, int arg2, int arg3)
     if (arg3 > result) result = arg3;
     return result;
 }
+//              0   1   2   3   4   5   6   7   8
+int [] array = {11, 21, 31, 41, 15, 611, 17, 18, 19};
+//array [0] = 12;  // задали значение массиву
+//Console.WriteLine(array[4]);  - попросили программу вывести на консоль значение массива 4 (15)
 
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
+int result = Max(
+    Max (array[0], array[1], array[2]),
+    Max (array[3], array[4], array[5]),
+    Max (array[6], array[7], array[8]));
 
-int a2 = 12;
-int b2 = 23;
-int c2 = 33;
-
-int a3 = 133;
-int b3 = 23;
-int c3 = 33;
-
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-
-int max = Max(max1, max2, max3);
-
-
-Console.WriteLine(max);
+Console.WriteLine(result);
