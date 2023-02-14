1a91f2046c381eafde1a5e61688e26d2ca5a3276
Asova417 <asova417@gmail.com>
2023-02-14 23:22:30 +0500

    Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.

---
+++ c#/seminar/Urok_02/Example_04/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+14 -> нет
+46 -> нет
+161 -> да
+
+Console.WriteLine("Enter your firstNumber");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber%7 == 0 && firstNumber%23 == 0)
+{Console.WriteLine("Your Number is good div");}
+else
+{Console.WriteLine("Your Number is NOT good div");}
