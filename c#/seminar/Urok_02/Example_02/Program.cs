95181b72b3ad67aef61a763346b697826acd11e3
Asova417 <asova417@gmail.com>
2023-02-14 22:54:40 +0500

    Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа. C примером решения от преподавателя

--- c#/seminar/Urok_02/Example_02/Program.cs
+++ c#/seminar/Urok_02/Example_02/Program.cs
@@ -4,10 +4,18 @@
 //782 -> 72
 //918 -> 98
 
+//int number = new Random().Next(100, 1000);
+//Console.WriteLine ($"Ваше случайное число: {number} ");
+
+//int a = number/100;
+//int b = number%10;
+
+//Console.WriteLine($"{a}{b}");
+
 int number = new Random().Next(100, 1000);
 Console.WriteLine ($"Ваше случайное число: {number} ");
+int digit1 = number / 100;
+int digit3 = number % 10;
 
-int a = number/100;
-int b = number%10;
-
-Console.WriteLine($"{a}{b}");
+int result = digit1 *10 + digit3;
+Console.Write($"Ваше получившееся число: {result}");
---
+++ c#/seminar/Urok_02/Example_03/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+//то программа выводит остаток от деления.
+
+//34, 5 -> не кратно, остаток 4 
+//16, 4 -> кратно
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Введите число: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+int a = number1 / number2;
+double b = number1 / number2;
+
+Console.WriteLine (a, b);
