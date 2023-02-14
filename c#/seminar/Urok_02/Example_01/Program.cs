dda7561f77a07b741da4aa5bc9ad7fccdd167a98
Asova417 <asova417@gmail.com>
2023-02-14 22:19:46 +0500

    Программа выдает рандомные числа от  10 до 99 и показывает максимум из двух чисел.

---
+++ c#/seminar/Urok_02/Example_01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа выдает рандомные числа от  10 до 99 и показывает максимум из двух чисел.
+//78->8
+//12->2
+//85->8
+
+int number = new Random().Next(10, 100);
+Console.WriteLine ($"Ваше случайное число: {number} ");
+
+int digit1 = number/10;
+int digit2 = number%10;
+
+if (digit1 > digit2)
+{
+    Console.Write($"Наибольшая цифра числа: {digit1}");
+}
+else if (digit1 < digit2)
+{
+    Console.Write($"Наибольшая цифра числа: {digit2}");
+}
+else
+{
+    Console.Write("Цифры равны");
+}
