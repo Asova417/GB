50317473c8bc95278d7113eb948a4de3c6ff357a
Asova417 <asova417@gmail.com>
2023-02-18 00:07:28 +0500

    Задача №18. Работа в группах.Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)

---
+++ c#/seminar/Urok_03/Example_02/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Задача №18. Работа в группах
+// // Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+
+// Console.Write("Введите число от 1 до 4: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// switch (n)
+// {
+//     case 1:
+//         Console.Write("X > 0 && Y < 0");
+//         break;
+
+
+//     case 2:
+//         Console.Write("X > 0 && Y > 0");
+//         break;
+
+
+//     case 3:
+//         Console.Write("X < 0 && Y < 0");
+//         break;
+
+
+//     case 4:
+//         Console.Write("X > 0 && Y < 0");
+//         break;
+//     default:
+//     Console.WriteLine("Ошибка");
+//     break;
+// }
+
