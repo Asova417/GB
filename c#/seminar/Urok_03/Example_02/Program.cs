6f60d7472b34f6b0a7aa9882f8ede2d7180d1f05
Asova417 <asova417@gmail.com>
2023-02-18 00:24:50 +0500

    Задача №18. Работа в группах.Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)

--- c#/seminar/Urok_03/Example_02/Program.cs
+++ c#/seminar/Urok_03/Example_02/Program.cs
@@ -1,6 +1,6 @@
 ﻿// // Задача №18. Работа в группах
 // // Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
-
+// Самостоятельное ужасное решение задачи.
 // Console.Write("Введите число от 1 до 4: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 
@@ -29,3 +29,34 @@
 //     break;
 // }
 
+int Promt(string message)
+{
+Console.Write(message);
+int result = Convert.ToInt32(Console.ReadLine());
+return result;
+}
+
+bool Checked(int quater)
+{
+if(quater >= 1 && quater <= 4)
+{
+return true;
+}
+else
+{
+return false;
+}
+}
+
+string[] quaters ={"x > 0; y > 0", "x < 0; y > 0", "x < 0; y < 0", "x > 0; y < 0"};
+
+int quater = Promt("Введите четверть: ");
+
+if(Checked(quater))
+{
+Console.Write(quaters[quater - 1]);
+}
+else
+{
+Console.Write("Неправильный номер четверти");
+}
