8742d2c2b4e7cbcfbdab6d55acaf224c5f447b6d
Asova417 <asova417@gmail.com>
2023-02-21 23:34:01 +0500

    Задачи. Варианты решения от преподавателя.

---
+++ c#/seminar/Urok_04/Reshenie_ot_AS/Program.cs
@@ -0,0 +1,41 @@
+﻿// // Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// // 456 -> 3
+// // 78 -> 2
+// // 89126 -> 5
+
+// int Counter(int number)
+// {
+// int count = 0;
+// if(number == 0)
+// {
+// count = 1;
+// }
+// while(number > 0)
+// {
+// number/=10; // == number = number/10
+// count++;
+// }
+// return count;
+// }
+
+// System.Console.Write("Введите число: ");
+// int A = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// Console.Write($"Количество цифр в числе = {Counter(A)}");
+
+Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+
+
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+System.Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+Random rnd = new Random();
+for(int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(0,2);
+}
+Console.WriteLine(string.Join(", ", arr));
