6acddfad28a9e666c0a63834a8136464787e0314
Asova417 <asova417@gmail.com>
2023-02-22 15:23:30 +0500

    Освоение методов на примере сложения 2 чисел путём ввода

--- c#/trash/01/Program.cs
+++ c#/trash/01/Program.cs
@@ -1,14 +1,32 @@
-﻿Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+﻿// Console.WriteLine("Введите число1: ");
+// int a1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число2: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
 
-static int Sum (int a, b)
+// static int Sum (int a, int b)
+// {
+//     int result = a + b;
+//     return result;
+// }
+
+// int c = Sum (a1, b1);
+// Console.WriteLine(c);
+
+int Nachalo(string message) //Метод ввода числа
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+static int Sum (int a, int b)//Метод расчета
 {
     int result = a + b;
     return result;
 }
 
-int c = Sum (a, b);
+int a1 = Nachalo("Введите первое число: ");
+int b1 = Nachalo("Введите второе число: ");
+int c = Sum (a1, b1);
 Console.WriteLine(c);
