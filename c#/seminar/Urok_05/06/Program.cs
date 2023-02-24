b6c9a0561b9bfdc64bbd0e0db0021f41ddc5053d
Asova417 <asova417@gmail.com>
2023-02-25 00:13:05 +0500

    Задача 35: Задайте одномерный массив из N случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].Примре решения от преподавателя.

--- c#/seminar/Urok_05/06/Program.cs
+++ c#/seminar/Urok_05/06/Program.cs
@@ -3,21 +3,55 @@
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
+//Решение в группе
+// int ArrSearch(int[] arry)
+// {
+//     int count = 0;
+//     for(int i = 0; i < arry.Length; i++)
+//     {
+//         if(arry[i] > 9 && arry[i] < 100)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
-int ArrSearch(int[] arry)
+// int[] arr = { 5, 18, 123, 6, 2};
+// Console.Write($"[{string.Join(", ", arr)}]");
+// int count = ArrSearch(arr);
+// Console.Write(count);
+
+// Решение от преподавателя.
+
+// Задача 35: Задайте одномерный массив из N случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] FillArrayWithRandomNumbers(int length)
 {
-    int count = 0;
-    for(int i = 0; i < arry.Length; i++)
-    {
-        if(arry[i] > 9 && arry[i] < 100)
-        {
-            count++;
-        }
-    }
-    return count;
+int[] arr = new int[length];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(0, 150);
+}
+return arr;
 }
 
-int[] arr = { 5, 18, 123, 6, 2};
-Console.Write($"[{string.Join(", ", arr)}]");
-int count = ArrSearch(arr);
-Console.Write(count);
+System.Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumbers(length);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+if(array[i] >= 10 && array[i] <= 99)
+{
+count++;
+}
+}
+System.Console.WriteLine(count);
