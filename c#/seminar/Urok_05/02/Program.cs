e9ebdfb6a6cb1dfc1573988458344ae6a3e6eb03
Asova417 <asova417@gmail.com>
2023-02-24 23:19:47 +0500

    Задача 32: Напишите программу замена элементов массива.

--- c#/seminar/Urok_05/02/Program.cs
+++ c#/seminar/Urok_05/02/Program.cs
@@ -27,8 +27,38 @@ int [] ArrManually(int lenght)
     return arr;
 }
 
-int lenght = Entry($"Введите длину массива");
-int [] array = ArrManually(lenght);
-Console.Write($"[{string.Join(", ", array)}]");
-array = ArrReplace (array);
-Console.Write($"[{string.Join(", ", array)}]");
+// int lenght = Entry($"Введите длину массива");
+// int [] array = ArrManually(lenght);
+// Console.Write($"[{string.Join(", ", array)}]");
+// array = ArrReplace (array);
+// Console.Write($"[{string.Join(", ", array)}]");
+
+
+// int[] FillArrayWithRandomNumbers(int length)
+// {
+// int[] arr = new int [length];
+// Random rnd = new Random();
+// for(int i =0; i < arr.Length; i++)
+// {
+// arr[i] = rnd.Next(-9, 10);
+// }
+// return arr;
+// }
+
+// int[] newMassive(int[] array)
+// {
+// for(int i = 0; i < array.Length; i++)
+// {
+// array[i] = -array[i]; // == array[i] = array[i] * -1// == array[i]*= -1
+// }
+// return array;
+// }
+
+// System.Console.WriteLine("Введите длину массива: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// int[] array = FillArrayWithRandomNumbers(length);
+// System.Console.WriteLine($"[{string.Join(", ", array)}]");
+// int[] newArray = newMassive(array);
+// System.Console.WriteLine($"[{string.Join(", ", newArray)}]");
+
+// // Решение от преподавателя
