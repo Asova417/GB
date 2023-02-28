6def3188b0b57a168e0ff160beae1f69b74068d7
Asova417 <asova417@gmail.com>
2023-02-28 23:40:55 +0500

    Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.

--- c#/seminar/Urok_06/Example_03/Program.cs
+++ c#/seminar/Urok_06/Example_03/Program.cs
@@ -53,3 +53,24 @@ BinaryView(number);
 // char[] charArray = arr.ToCharArray();
 // Array.Reverse(charArray);
 // Console.WriteLine(string.Join("",charArray));
+
+//Решение от Мухамеда
+
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+// int number = 0;
+// string str = String.Empty;
+
+// Console.WriteLine("Введите десятичное число");
+// number = Convert.ToInt32(Console.ReadLine());
+
+// while ( number > 0)
+// {
+//     str = Convert.ToString(number%2) + str;
+//     number = number / 2;
+    
+// }
+// Console.WriteLine(str);
