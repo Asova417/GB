c3150612bf8d59298be7d838679de3161c62b3c1
Asova417 <asova417@gmail.com>
2023-03-04 00:01:41 +0500

    Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали.

--- c#/seminar/Urok_07/04/Program.cs
+++ c#/seminar/Urok_07/04/Program.cs
@@ -6,54 +6,112 @@
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
-int[,] ArrayWithRandom(int row, int column)
-{
-    int[,] arr = new int[row, column];
+// int[,] ArrayWithRandom(int row, int column)
+// {
+//     int[,] arr = new int[row, column];
     
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for(int j = 0; j < arr.GetLength(1); j++)
-        {
-            arr[i,j] = new Random().Next(1, 10);
-        }
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = new Random().Next(1, 10);
+//         }
         
+//     }
+//     return arr;
+// }
+
+// int Sum(int[,] matrix)
+// {
+//     int sum = 0;
+//     for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if(i == j)
+//             {
+//                 sum = sum + matrix[i,j];
+//             }
+            
+//         }
+//     }
+//     return sum;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = ArrayWithRandom(row, column);
+// PrintMatrix(matrix);
+// Console.WriteLine();
+// Console.Write($" {Sum(matrix)}");
+
+// Решение от преподавателя.
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(2, 10);
+        }
     }
-    return arr;
+    return matrix;
 }
 
 int Sum(int[,] matrix)
 {
     int sum = 0;
-    for(int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for(int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if(i == j)
-            {
-                sum = sum + matrix[i,j];
-            }
-            
+            if (i == j)
+                {
+                sum += matrix[i, j];
+                }
         }
     }
-    return sum;
-}
-void PrintMatrix(int[,] matrix)
-{
-    for(int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for(int j = 0; j < matrix.GetLength(1); j++)
-        {
-            System.Console.Write(matrix[i,j] + " ");
-        }
-        Console.WriteLine();
-    }
+return sum;
 }
 
-Console.Write("Введите кол-во строк: ");
-int row = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
-int column = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = ArrayWithRandom(row, column);
-PrintMatrix(matrix);
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write(matrix[i, j] + " ");
+}
 Console.WriteLine();
-Console.Write($" {Sum(matrix)}");
+}
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+System.Console.WriteLine(Sum(matrix));
