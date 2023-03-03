f7ac51be69ca4829a30cdbc9f20db361ccff6585
Asova417 <asova417@gmail.com>
2023-03-03 23:38:12 +0500

    Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.

--- c#/seminar/Urok_07/03/Program.cs
+++ c#/seminar/Urok_07/03/Program.cs
@@ -1,50 +1,102 @@
-﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
-// и замените эти элементы на их квадраты.
+﻿// // Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// // и замените эти элементы на их квадраты.
 
-int[,] ArrayWithRandom(int row, int column)
-{
-    int[,] arr = new int[row, column];
+// int[,] ArrayWithRandom(int row, int column)
+// {
+//     int[,] arr = new int[row, column];
     
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for(int j = 0; j < arr.GetLength(1); j++)
-        {
-            arr[i,j] = new Random().Next(1, 10);
-        }
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = new Random().Next(1, 10);
+//         }
         
-    }
-    return arr;
-}
+//     }
+//     return arr;
+// }
 
-int [,] Rewrite(int[,] matrix)
-{
-    for(int i = 0; i < matrix.GetLength(0); i+=2)
-    {
-        for(int j = 0; j < matrix.GetLength(1); j+=2)
-        {
-            matrix[i,j] = matrix[i, j] * matrix[i, j];
+// int [,] Rewrite(int[,] matrix)
+// {
+//     for(int i = 0; i < matrix.GetLength(0); i+=2)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j+=2)
+//         {
+//             matrix[i,j] = matrix[i, j] * matrix[i, j];
             
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for(int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             System.Console.Write(matrix[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите кол-во строк: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = ArrayWithRandom(row, column);
+// PrintMatrix(matrix);
+// Console.WriteLine();
+// PrintMatrix(Rewrite(matrix));
+
+//Решение от преподавателя
+// Задача 49: Задайте двумерный массив.
+// Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(2, 10);
         }
     }
     return matrix;
 }
+
+int[,] NewMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+    return matrix;
+}
+
 void PrintMatrix(int[,] matrix)
 {
-    for(int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for(int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            System.Console.Write(matrix[i,j] + " ");
+            System.Console.Write(matrix[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
 
-Console.Write("Введите кол-во строк: ");
+System.Console.Write("Введите кол-во строк: ");
 int row = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
+System.Console.Write("Введите кол-во столбцов: ");
 int column = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = ArrayWithRandom(row, column);
+int[,] matrix = FillMatrixWithRandom(row, column);
 PrintMatrix(matrix);
-Console.WriteLine();
-PrintMatrix(Rewrite(matrix));
+int[,] newMatrix = NewMatrix(matrix);
+System.Console.WriteLine();
+PrintMatrix(newMatrix);
